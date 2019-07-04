9e4b99d7a5f5aae8c4eacecf9fdddd0ff4078eef
Vladimir Tosev <vladimir.tosev@etondigital.com>
2019-07-04 18:08:55 +0200

    Fix php 7.3 error for break continue

--- lib/Doctrine/ORM/UnitOfWork.php
+++ lib/Doctrine/ORM/UnitOfWork.php
@@ -2633,7 +2633,7 @@ class UnitOfWork implements PropertyChangedListener
                         $class->reflFields[$field]->setValue($entity, $data[$field]);
                         $this->originalEntityData[$oid][$field] = $data[$field];
 
-                        continue;
+                        break;
                     }
 
                     $associatedId = array();
